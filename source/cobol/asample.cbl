1ab8a3eadacd4eef3ae7502560ab7a80db074bb9
Nelson Lopez <Nelson.Lopez1@ibm.com>
2026-06-07 21:18:58 -0400

    doc sample tested and ready

---
+++ source/cobol/asample.cbl
@@ -0,0 +1,12 @@
+000100 ID DIVISION.     
+                 
+       PROGRAM-ID. ASAMPLE.
+       ENVIRONMENT DIVISION.                              
+       DATA DIVISION.                                     
+       WORKING-STORAGE SECTION.   
+      * 
+       COPY MYVARS.                                      
+       PROCEDURE DIVISION.                                
+
+           DISPLAY 'Demo Z Devops pgm.' 
+           STOP RUN.   
---
+++ source/jcl/asample_job_for_demo.jcl
@@ -0,0 +1,7 @@
+//GUEST1A JOB CLASS=A,MSGCLASS=H,MSGLEVEL=(1,1),REGION=0M
+//*
+//*  Test a sample cobol pgm build in VS Code 
+//* 
+//DEMO     EXEC PGM=ASAMPLE 
+//STEPLIB  DD  DISP=SHR,DSN=GUEST1.VSCODE.LOAD  HLQ for builds
+//SYSOUT   DD SYSOUT=*
